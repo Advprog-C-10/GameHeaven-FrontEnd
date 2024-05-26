2176d0b63da979213d992d436c3f5df0563d28b3
MahogaHeryasa <hery.heryasa@gmail.com>
2024-05-26 22:17:37 +0700

    add navigation to cart page button

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1>Home Page</h1>
+    <><h1>Home Page</h1>
+      <Link href="/cart" legacyBehavior>
+        <a>
+          <button className="bg-gray-500 text-white px-4 py-2 rounded">Go to Cart</button>
+        </a>
+      </Link></>
   );
 }
